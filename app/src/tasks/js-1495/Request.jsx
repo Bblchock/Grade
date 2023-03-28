56425218f03a3881720b8fe1e6a1aebc352b5e6a
Лебаков Кирилл <bblchock.p.izdat@gmail.com>
2023-03-28 14:15:56 +0300

    Правки js

--- app/src/tasks/js-1495/Request.jsx
+++ app/src/tasks/js-1495/Request.jsx
@@ -4,31 +4,37 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 const Request = () => {
-  const [items, setitems] = useState();
+  const [items, setItems] = useState();
   const [error, setError] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     axios
       .get("https://62e90ee0249bb1284eb96781.mockapi.io/items")
-      .then(({ data }) => {
-        setitems(data);
-        setError(undefined);
+      .then(response => {
+        if (response.status === 200) {
+          setItems(response.data);
+          setError(undefined);
+        }
       })
       .catch(err => {
-        setError(`Ошибка: ${err.response.data}`);
-        setitems(undefined);
+        setError(err.response.data);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <div className="items">
       Request
-      {items ? (
-        items.map(item => <div key={item.id}>{item.title}</div>)
+      {error || isLoading ? (
+        <div className="error">
+          {error ? `Ошибка: ${error}` : "Ожидаем ответ сервера"}
+        </div>
       ) : (
-        <div>'Ожидаем ответ с сервака'</div>
+        items?.slice(0, 3).map(item => <div key={item.id}>{item.title}</div>)
       )}
-      {error && <div className="error">{error}</div>}
     </div>
   );
 };
